8ecbc4058e9c57c3994179fd1dc2108e509d5c8b
Dome C <dome@tel.co.th>
2020-11-08 18:47:23 +0700

    Add Exchange

---
+++ exchange/README.md
@@ -0,0 +1,25 @@
+# SushiSwap Classic Exchange Interface
+
+Forked from [Uniswap/uniswap-interface](https://github.com/Uniswap/uniswap-interface), and updated to work with SushiSwap contracts.
+
+## Accessing the SushiSwap Classic Exchange Interface
+
+To access the SushiSwap Exchnage, visit [exchange.sushiswapclassic.org](https://exchange.sushiswapclassic.org/#/).
+
+## Development
+
+### Install Dependencies
+
+```bash
+yarn
+```
+
+### Run
+
+```bash
+yarn start
+```
+
+## Contributions
+
+**Please open all pull requests against the `master` branch.** 
---
+++ exchange/fork/sdk/src/constants.ts
@@ -0,0 +1,53 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+  BSC_MAINNET = 56,
+  BSC_TESTNET = 97,
+  TC_MAINNET = 33,
+  TC_TESTNET = 32
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0xA28D86bf172f752433808Cd687D622a0E8ffe468'
+export const INIT_CODE_HASH = '0x9a461d405506a6a3ba2275d96db3ca4e657bc8736cc4e72725b13f9d18d3e4aa'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
